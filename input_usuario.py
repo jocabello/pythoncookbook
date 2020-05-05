5feaa86ce8d8663b381c0f73a96cb5c843a2a3cd
jocabello <jose.icc87@gmail.com>
2020-05-05 11:25:04 -0400

    User input por consola y tipos de variables

---
+++ input_usuario.py
@@ -0,0 +1,7 @@
+nombre = input("Ingresar tu nombre: ")
+
+print("Hola: " + nombre + "!")
+
+numero = input("Ingresa un número: ")
+
+print("Tu número multiplicado por 5 es: " + str(int(numero) * 5))  # int(numero) esto se denomina typecasting
---
+++ tipo_de_variables.py
@@ -0,0 +1,10 @@
+mi_int = 3
+mi_float = 2.2124
+mi_str = "Esto es un string"
+
+print(type(mi_int))
+print(type(mi_float))
+print(type(mi_str))
+
+mi_int = "Ahora esto es un str"
+print(type(mi_int))
